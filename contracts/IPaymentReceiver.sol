db7ae5f2325054e4685482a4a233429d1b6eebd2
Kenk <kenghin_lim@hotmail.com>
2022-05-11 10:16:10 +0800

    feat: Add Payment Receiver Interface

---
+++ contracts/IPaymentReceiver.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "hardhat/console.sol";
+
+interface IPaymentReceiver {
+    /**
+     * @dev Check into the client and start streaming payment
+     */
+    function checkIn(uint256 clientId) external;
+
+    /**
+     * @dev Check out of the client and stop streaming payment
+     */
+    function checkOut(uint256 clientId) external;
+}
