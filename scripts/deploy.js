605d3a2555a5abed92809397070032d10079b86d
Owen <oschleic@asu.edu>
2022-05-16 23:14:01 -0700

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,10 +14,14 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const PaymentReceiver = hre.ethers.getContractFactory("PaymentReceiver");
-  const pr = await PaymentReceiver.deploy();
+  const PaymentReceiver = await hre.ethers.getContractFactory("PaymentReceiver");
+  const host = "0xEB796bdb90fFA0f28255275e16936D25d3418603";
+  const cfa = "0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873";
+  const pr = await PaymentReceiver.deploy(host, cfa);
 
-  console.log("Payment Receiver deployed at:", greeter.address);
+  await pr.deployed();
+  
+  console.log("Payment Receiver deployed at:", pr.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
